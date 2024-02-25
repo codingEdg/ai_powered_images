6aa690e27785a95f5374346499b04779449aeeb2
Mehtab Ali <mohd2mehtab@gmail.com>
2024-02-26 00:16:50 +0530

    clerk authentication added

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,18 @@
+// import Sidebar from "@/components/shared/Sidebar";
+import React from "react";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  console.log("layout in root folder");
+  return (
+    <main className="root">
+      {/* <Sidebar /> */}
+      {/* <MobileNavi /> */}
+
+      <div className="root-container">
+        <div className="wrapper">{children}</div>
+      </div>
+    </main>
+  );
+};
+
+export default Layout;
---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+const Home = () => {
+  return (
+    <main>
+      <h3>Home page</h3>
+      <UserButton afterSignOutUrl="/" />
+    </main>
+  );
+};
+
+export default Home;
