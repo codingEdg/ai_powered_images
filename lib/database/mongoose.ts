75de1278e561b141288c0b1b901ea8f8e565afd3
Mehtab Ali <mohd2mehtab@gmail.com>
2024-02-29 16:16:51 +0530

    test

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -24,7 +24,7 @@ export const connectToDatabase = async () => {
   cached.promise =
     cached.promise ||
     mongoose.connect(MONGODB_URL, {
-      dbName: "Imaginify",
+      dbName: "imaginify",
       bufferCommands: false,
     });
 
