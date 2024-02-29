2bee7956f3f09ac3644e7ec169c21ee70857c820
Mehtab Ali <mohd2mehtab@gmail.com>
2024-02-29 17:20:45 +0530

    .z

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -7,7 +7,7 @@ import { Webhook } from "svix";
 
 import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
 
-console.log("this is route.ts file from vs code");
+alert("this is route.ts file from vs code");
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
